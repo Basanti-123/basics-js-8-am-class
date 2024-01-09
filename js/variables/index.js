58c25e134743fb809decb62eef590b10808a7ecc
BasantiNagari <basantinagari23@gmail.com>
2024-01-09 19:32:14 +0545

    variables and intraction learned

--- js/variables/index.js
+++ js/variables/index.js
@@ -1,1 +1,41 @@
 console.log("Hello World")
+
+// hello word (single line comment)
+
+/* (multi line comment)
+h
+s
+as
+as
+*/
+ var person = "basanti";   //contsiner = data
+ var fullName = "basanti Nagari";
+ var isAdult = "yes";
+ // =========
+ const personName = "ramita";
+
+ // var/ let = data replace garna
+ // const = constant variable
+
+ // Hosting 
+
+ const phone = "1234";
+ console.log(person);
+
+ // intractions
+
+ // promt, confirm, alert
+
+ // alert notification
+
+ alert("Good morning Basanti");
+
+ // prompt = Questionnaire
+
+ const age = prompt("How old aru you?");
+ console.log(age);
+ 
+
+ // confirm = yes/no question
+ const isValid = confirm("Are you above 18 years?");
+ console.log(isValid)
